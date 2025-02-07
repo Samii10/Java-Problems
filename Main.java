77fb794c1ea076a2d0d27284160545ae49b74d86
Md Golam Bin As-Sami <144956960+Samii10@users.noreply.github.com>
2025-02-08 02:27:59 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        /*int[] number= new int[5];
+
+
+        number[0]= 10;
+        number[1]= 22;
+        number[2]= 10;
+        number[3]= 10;
+        number[4]= 10;
+
+        int sum=number[0]+number[1]+number[3]+number[4]+number[2];
+
+        System.out.println("Sum: "+sum);
+        int len= number.length;
+        System.out.println(number[1]);
+        System.out.println("Array size:"+len);*/
+    }
+}
